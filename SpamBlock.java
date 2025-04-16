73817009ce10e2faccfaea6c4fec41b70d5a427d
Sarah <339710410@gotvdsb.ca>
2025-04-16 13:26:34 -0400

    updated block class

--- SpamBlock.java
+++ SpamBlock.java
@@ -1,3 +1,11 @@
 public class SpamBlock extends Block {
-    
+    public SpamBlock(String level, String button, int enterTime, int receiveTime) {
+        super(level, button, enterTime, receiveTime);
+    }
+
+    @Override
+    boolean receive() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'receive'");
+    }
 }
